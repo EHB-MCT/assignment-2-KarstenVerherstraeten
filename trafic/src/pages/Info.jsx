bdd96b6d8798afc22fbb7cb107ef98fef909c922
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-12-24 12:12:38 +0100

    feat: create Info component with Navbar and informational content

---
+++ trafic/src/pages/Info.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+function Info () {
+  return (
+    <>
+    <Navbar />
+      <h1>Info</h1>
+      <p>Info about the data</p>
+    </>
+  );
+}
+
+export default Info;
