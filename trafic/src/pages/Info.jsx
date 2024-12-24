4fb4b85dd7eff54908e230531dc0fa85233b050f
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-12-24 12:22:53 +0100

    chore: added my reference in info.jsx

--- trafic/src/pages/Info.jsx
+++ trafic/src/pages/Info.jsx
@@ -5,6 +5,7 @@ function Info () {
     <>
     <Navbar />
       <h1>Info</h1>
+      <p>idea is based of: <a href="https://www.worldometers.info/">Worldometers.info</a></p>
       <p>Info about the data</p>
     </>
   );
