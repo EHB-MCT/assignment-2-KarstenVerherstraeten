dc94d4453a60510de7aa64bad2c490476ca3e785
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-12-24 12:11:54 +0100

    feat: add Info component and route for /info path

--- trafic/src/App.js
+++ trafic/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import PageNotFound from "./pages/404.jsx";
+import Info from "./pages/Info.jsx";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 		<Router>
 			<Routes>
 				<Route path="/" element={<Home />} />
+        <Route path="/info" element={<Info/>}/>
 				<Route path="*" element={<PageNotFound />} /> {/* 404 page */}
 			</Routes>
 		</Router>
