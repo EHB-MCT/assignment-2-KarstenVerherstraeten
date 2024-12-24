4cf7b05a819a358bccbccdbe82a02aacb499de45
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-12-24 12:05:32 +0100

    feat: implement routing with react-router-dom and add Home and 404 pages

--- trafic/src/App.js
+++ trafic/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import PageNotFound from "./pages/404.jsx";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<PageNotFound />} /> {/* 404 page */}
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
