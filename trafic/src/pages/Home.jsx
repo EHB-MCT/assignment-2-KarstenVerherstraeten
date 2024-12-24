7d1a3858ca0e18e70a7d79fe312cd24c1aa4e982
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-12-24 12:22:11 +0100

    chore: added basic description of what my project will be

--- trafic/src/pages/Home.jsx
+++ trafic/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Navbar from '../components/Navbar.jsx';
+import { Link } from 'react-router-dom';
 function Home() {
 	return (
 		<>
          <Navbar />
 			<h1>Trafic Data</h1>
+            <p>
+                Welcome to the visualisation of belgium police data. 
+                for more info about the data I use:<Link to="/info">Click here</Link>.
+            </p>
+
+            <p>
+    The goal of this project is to make data from the Belgian police easier to understand by showing how often accidents happen in Belgium.  
+    Using the latest police statistics, the data is visualized in a way that helps people see the patterns and frequency of these accidents over time.  
+</p>
 		</>
 	);
 }
