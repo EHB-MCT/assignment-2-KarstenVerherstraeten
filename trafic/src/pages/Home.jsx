0d4d7b610538d3a7d781d2b59cf36d6dcf5092e5
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-12-26 12:34:33 +0100

    feat: wrap Counter component with DataProvider for data context integration

--- trafic/src/pages/Home.jsx
+++ trafic/src/pages/Home.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import Navbar from '../components/Navbar.jsx';
-import { Link } from 'react-router-dom';
-import Counter from '../components/Counter.jsx';
+import React from "react";
+import Navbar from "../components/Navbar.jsx";
+import { Link } from "react-router-dom";
+import Counter from "../components/Counter.jsx";
+import { DataProvider } from "../components/DataContext.jsx";
 function Home() {
 	return (
 		<>
-         <Navbar />
+			<Navbar />
 			<h1>Trafic Data</h1>
-            <p>
-                Welcome to the visualisation of belgium police data. 
-                for more info about the data I use:<Link to="/info">Click here</Link>.
-            </p>
+			<p>
+				Welcome to the visualisation of belgium police data. for more info about
+				the data I use:<Link to="/info">Click here</Link>.
+			</p>
 
-            <p>
-    The goal of this project is to make data from the Belgian police easier to understand by showing how often accidents happen in Belgium.  
-    Using the latest police statistics, the data is visualized in a way that helps people see the patterns and frequency of these accidents over time.  
-</p>
+			<p>
+				The goal of this project is to make data from the Belgian police easier
+				to understand by showing how often accidents happen in Belgium. Using
+				the latest police statistics, the data is visualized in a way that helps
+				people see the patterns and frequency of these accidents over time.
+			</p>
 
-<Counter />
+			<DataProvider>
+				<Counter />
+			</DataProvider>
 		</>
 	);
 }
