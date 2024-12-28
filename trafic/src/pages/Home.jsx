27058e59aa6396655ff1babbc5488293a5a3c834
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-12-28 12:57:46 +0100

    feat: add CSS module for Home component and update layout

--- trafic/src/pages/Home.jsx
+++ trafic/src/pages/Home.jsx
@@ -3,17 +3,20 @@ import Navbar from "../components/Navbar.jsx";
 import { Link } from "react-router-dom";
 import Counter from "../components/Counter.jsx";
 import { DataProvider } from "../components/DataContext.jsx";
+import style from "../modules/home.module.css";
+
 function Home() {
 	return (
 		<>
 			<Navbar />
-			<h1>Trafic Data</h1>
-			<p>
-				Welcome to the visualisation of belgium police data. for more info about
-				the data I use:<Link to="/info">Click here</Link>.
+			<div className={style.home}>
+			<p className={style.left}>
+
+				Welcome to my project! I am a student who tried to make a better visualisation of frequency of the trafic violations in belgium.
+				You can see the data in the form of a counter, which shows the number of accidents that have happened in Belgium over the years.
 			</p>
 
-			<p>
+			<p className={style.right}>
 				The goal of this project is to make data from the Belgian police easier
 				to understand by showing how often accidents happen in Belgium. Using
 				the latest police statistics, the data is visualized in a way that helps
@@ -23,6 +26,8 @@ function Home() {
 			<DataProvider>
 				<Counter />
 			</DataProvider>
+			</div>
+			
 		</>
 	);
 }
